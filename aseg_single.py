bd8d107de22cb5f7df7a2af340ce011d757b070c
William A. Romero R <contact@waromero.com>
2023-05-02 15:47:11 +0200

    Fix PrintMetrics() call

--- aseg_single.py
+++ aseg_single.py
@@ -46,4 +46,4 @@ if __name__ == '__main__':
 .   ----------------------------------------------------------------------------
     """    
     aSegmentation.Compute()
-    aSegmentation.PrintMetrics()
+    aSegmentation.PrintSingleMetrics()
